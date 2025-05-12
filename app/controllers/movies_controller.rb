c735ea4181401254b796e29d6fa6e5d96e5eb980
Ian Heraty <heratyian@gmail.com>
2025-05-12 14:12:06 -0500

    Refactored movie form to use new and edit routes. Created form partial. Refactored the movies controller.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,66 +8,65 @@ class MoviesController < ApplicationController
   end
 
   def show
-    the_id = params.fetch("path_id")
+    the_id = params.fetch("id")
 
     matching_movies = Movie.where({ :id => the_id })
 
-    @the_movie = matching_movies.at(0)
+    @movie = matching_movies.at(0)
 
     render({ :template => "movies/show" })
   end
 
   def new
-    # TODO: render template
+    @movie = Movie.new
+
+    render({ :template => "movies/new" })
   end
 
   def create
-    the_movie = Movie.new
-    the_movie.title = params.fetch("query_title")
-    the_movie.year = params.fetch("query_year")
-    the_movie.director_id = params.fetch("query_director_id")
+    @movie = Movie.new(movie_params)
 
-    if the_movie.valid?
-      the_movie.save
+    if @movie.valid?
+      @movie.save
       redirect_to("/movies", { :notice => "Movie created successfully." })
     else
-      redirect_to("/movies", { :alert => the_movie.errors.full_messages.to_sentence })
+      render({ :template => "movies/new" })
     end
   end
 
   def edit
-    the_id = params.fetch("path_id")
+    the_id = params.fetch("id")
 
     matching_movies = Movie.where({ :id => the_id })
 
-    @the_movie = matching_movies.at(0)
+    @movie = matching_movies.at(0)
 
     render({ :template => "movies/edit" })
   end
 
   def update
-    the_id = params.fetch("path_id")
-    @the_movie = Movie.where({ :id => the_id }).at(0)
+    the_id = params.fetch("id")
+    @movie = Movie.where({ :id => the_id }).at(0)
 
-    @the_movie.title = params.fetch("query_title")
-    @the_movie.year = params.fetch("query_year")
-    @the_movie.director_id = params.fetch("query_director_id")
-
-    if @the_movie.valid?
-      @the_movie.save
-      redirect_to("/movies/#{@the_movie.id}", { :notice => "Movie updated successfully."} )
+    if @movie.update(movie_params)
+      redirect_to("/movies/#{@movie.id}", { :notice => "Movie updated successfully."} )
     else
       render({ :template => "movies/edit" })
-      # redirect_to("/movies/#{the_movie.id}", { :alert => the_movie.errors.full_messages.to_sentence })
     end
   end
 
   def destroy
-    the_id = params.fetch("path_id")
-    the_movie = Movie.where({ :id => the_id }).at(0)
+    the_id = params.fetch("id")
+    movie = Movie.where({ :id => the_id }).at(0)
 
-    the_movie.destroy
+    movie.destroy
 
     redirect_to("/movies", { :notice => "Movie deleted successfully."} )
+  end
+
+  private
+
+  def movie_params
+    params.require("movie").permit(:title, :year, :director_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -40,20 +40,7 @@ Rails.application.routes.draw do
   # Routes for the Movie resource:
 
   # CREATE
-  post("/insert_movie", { :controller => "movies", :action => "create" })
-
-  # READ
-  get("/movies", { :controller => "movies", :action => "index" })
-
-  get("/movies/:path_id", { :controller => "movies", :action => "show" })
-
-  # UPDATE
-  get("/movies/:path_id/edit", { :controller => "movies", :action => "edit" })
-  post("/modify_movie/:path_id", { :controller => "movies", :action => "update" })
-
-  # DELETE
-  get("/delete_movie/:path_id", { :controller => "movies", :action => "destroy" })
-
+  resources :movies
   #------------------------------
 
   # Routes for the Director resource:
