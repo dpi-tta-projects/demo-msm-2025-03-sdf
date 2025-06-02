969a0ce5e4f9aedae99217669fb15f7779679f81
Ian Heraty <heratyian@gmail.com>
2025-06-02 14:22:58 -0500

    Revert movies#index per_page to default 25

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.order({ :created_at => :desc }).page(params[:page]).per(10)
+    @movies = Movie.order({ :created_at => :desc }).page(params[:page])
   end
 
   def show
